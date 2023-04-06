56889b20cc9a58ee16dbb5496a717ae161dc8674
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-06 10:53:34 +0200

    setup basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import AddACat from './pages/AddACat';
+import CatGallery from './pages/CatGallery';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <div className="text-3xl font-bold">
-      I WILL DO THIS!!
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/cat-gallery" element={<CatGallery />} />
+                <Route path="/add-a-cat" element={<AddACat />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
