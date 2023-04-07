ff1238d544b2c3b23eab71fe37667032eedd85f3
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-07 14:04:39 +0200

    create component with an api call for selecting a breed

---
+++ src/components/SelectBreed.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import CatProperty from './CatProperty';
+import { Button } from './Button';
+import Loader from './Loader/Loader';
+
+export interface CatBreed {
+    id: string;
+    name: string;
+    temperament: string;
+    wikipedia_url: string;
+    description: string;
+    origin: string;
+    image: {
+        url: string;
+    };
+}
+
+const SelectBreed: React.FunctionComponent = () => {
+    const [breeds, setBreeds] = useState<CatBreed[]>([]);
+    const [selectedBreed, setSelectedBreed] = useState<CatBreed | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function getBreeds() {
+        setIsLoading(true);
+
+        try {
+            const response = await fetch('https://api.thecatapi.com/v1/breeds', {
+                headers: {
+                    'x-api-key': `${process.env.REACT_APP_CAT_API_KEY}`
+                }
+            });
+            const data: CatBreed[] = await response.json();
+            //filter to only include breeds with an image
+            const filteredBreeds = data.filter((img) => img.image?.url != null);
+            setBreeds(filteredBreeds);
+            setSelectedBreed(filteredBreeds[0]);
+            setIsLoading(false);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        getBreeds();
+    }, []);
+
+    const handleBreedChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedBreed(breeds[Number(event.target.value)]);
+    };
+
+    return (
+        <>
+            {isLoading && <Loader />}
+            <div className="flex justify-center w-full">
+                <span className="border-2 border-dark-green h-fit mt-2 p-2 rounded">
+                    <label className="text-2xl mr-2" htmlFor="breed_selector">
+                        Select a breed:
+                    </label>
+                    <select className="text-2xl" id="breed_selector" onChange={handleBreedChange} value={breeds.indexOf(selectedBreed as CatBreed)}>
+                        {breeds.map((breed: CatBreed, index: number) => (
+                            <option key={index} value={index}>
+                                {breed.name}
+                            </option>
+                        ))}
+                    </select>
+                </span>
+            </div>
+
+            {selectedBreed && (
+                <div className="flex justify-between">
+                    <div className="w-[46%] mt-12">
+                        <img className="rounded" src={selectedBreed.image.url} alt={selectedBreed.name} />
+                    </div>
+                    <div className="w-[46%] border-2 border-dark-green rounded p-4 mt-12">
+                        <CatProperty label="Breed" value={selectedBreed.name} />
+
+                        <CatProperty label="Country of origin" value={selectedBreed.origin} />
+
+                        <CatProperty label="Description" value={selectedBreed.description} />
+
+                        <CatProperty label="Temperament" value={selectedBreed.temperament} />
+
+                        <CatProperty
+                            label="Learn More"
+                            value={
+                                <Button target="blank" variant="link" as="a" href={selectedBreed.wikipedia_url}>
+                                    Wikipedia
+                                </Button>
+                            }
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default SelectBreed;
