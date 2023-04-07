80a2950ac4fef96a4a2046a3d25ccdc312670817
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-07 21:39:32 +0200

    refactor width and height

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import catLoader from './catLoader.gif';
 
 export default function Loader() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center bg-white">
+        <div className="flex w-full mt-24 items-center justify-center bg-white">
             <img src={catLoader} height="80px" width="80px" alt="loading..." />
         </div>
     );
