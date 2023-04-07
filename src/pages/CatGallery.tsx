ee158e98383b31eda2d652b28a4e3cfd632a20eb
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-07 20:34:06 +0200

    change call to breeds. add implement layout component and wrapper div.

--- src/pages/CatGallery.tsx
+++ src/pages/CatGallery.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import CatCard from '../components/CatCard';
 import Loader from '../components/Loader/Loader';
-
-export interface CatGalleryProps {}
+import { CatBreed } from '../components/SelectBreed';
+import PageTitle from '../components/PageTitle';
+import LayoutContainer from '../components/LayoutContainer';
 
 export interface Cat {
     id: string;
@@ -10,16 +11,8 @@ export interface Cat {
     breeds: CatBreed[];
 }
 
-export interface CatBreed {
-    name?: string;
-    temperament?: string;
-    origin?: 'string';
-    wikipedia_url?: string;
-    description?: string;
-}
-
-const CatGallery: React.FunctionComponent<CatGalleryProps> = () => {
-    const [cats, setCats] = useState([]);
+const CatGallery: React.FunctionComponent = () => {
+    const [cats, setCats] = useState<Cat[]>([]);
     const [isLoading, setIsLoading] = useState(false);
 
     async function getCats() {
@@ -27,13 +20,13 @@ const CatGallery: React.FunctionComponent<CatGalleryProps> = () => {
         setIsLoading(true);
 
         try {
-            const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=12', {
+            const response = await fetch('https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=12', {
                 headers: {
                     'x-api-key': `${process.env.REACT_APP_CAT_API_KEY}`
                 }
             });
-            const json = await response.json();
-            setCats(json);
+            const data: Cat[] = await response.json();
+            setCats(data);
             setIsLoading(false);
         } catch (e) {
             console.log(e);
@@ -44,17 +37,18 @@ const CatGallery: React.FunctionComponent<CatGalleryProps> = () => {
         getCats();
     }, []);
 
-    console.log(cats);
-
     return (
         <>
             {isLoading && <Loader />}
-            <div className="mx-auto flex w-9/12 flex-wrap py-10">
-                {cats &&
-                    cats.map((cat: Cat) => {
-                        return <CatCard cat={cat} key={cat.id} />;
-                    })}
-            </div>
+            <LayoutContainer>
+                <PageTitle title="Cat Gallery" />
+                <div className="flex mx-auto flex-wrap">
+                    {cats &&
+                        cats.map((cat: Cat) => {
+                            return <CatCard cat={cat} key={cat.id} />;
+                        })}
+                </div>
+            </LayoutContainer>
         </>
     );
 };
