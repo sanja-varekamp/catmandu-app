0099d983dcfb0ab9d3d9813cc812dd2f48b81fd5
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-08 12:47:54 +0200

    improve card for responsiveness. add the cat breed as an alt to the image

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -10,14 +10,14 @@ export interface CatCardProps {
 const CatCard: React.FunctionComponent<CatCardProps> = ({ cat }) => {
     const breed = cat.breeds[0] || {};
     return (
-        <div className="border-[4px] border-light-green rounded w-[400px] h-[430px] flex m-5 flex-col justify-between">
+        <div className="border-[4px] border-light-green rounded w-[400px] min-h-[430px] flex m-5 flex-col justify-between">
             <div className="h-[50px] bg-transparent-pink flex">
                 <p className="text-2xl p-2 ">{breed.name}</p>
             </div>
             <div className="items-center flex h-[300px]">
-                <img className="h-full min-w-full object-cover" src={cat.url} alt="Cute cat" />
+                <img className="h-full min-w-full object-cover" src={cat.url} alt={breed.name} />
             </div>
-            <div className="h-[80px] bg-transparent-green flex items-center">
+            <div className="min-h-[80px] bg-transparent-green flex items-center ">
                 <span className="px-2">
                     <FontAwesomeIcon icon={faCat} size="2xl" />
                 </span>
