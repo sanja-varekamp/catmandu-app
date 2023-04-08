b24836e7c3529896c4a963ffd00d16b21839e587
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-08 12:47:16 +0200

    navabar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LayoutContainer from './LayoutContainer';
+
+export default function Navbar() {
+    return (
+        <LayoutContainer>
+            <div className="flex h-20 border-2 border-dark-green bg-transparent-green justify-between text-3xl w-full items-center "></div>
+        </LayoutContainer>
+    );
+}
