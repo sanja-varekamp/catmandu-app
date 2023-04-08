fbf81f31ed3ea327e2ea38c6de45f0e0eb210521
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-08 23:19:02 +0200

    create a component that displays the uploaded cats. first draft

---
+++ src/components/MyCats.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import CatCard from '../components/CatCard';
+import LayoutContainer from './LayoutContainer';
+import Loader from './Loader/Loader';
+import { Cat } from '../pages/CatGallery';
+
+const MyCats: React.FunctionComponent = () => {
+    const [myCats, setMyCats] = useState<Cat[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function getCats() {
+        setMyCats([]);
+        setIsLoading(true);
+
+        try {
+            const response = await fetch('https://api.thecatapi.com/v1/images', {
+                headers: {
+                    'x-api-key': `${process.env.REACT_APP_CAT_API_KEY}`
+                }
+            });
+            const data: Cat[] = await response.json();
+            setMyCats(data);
+            setIsLoading(false);
+        } catch (e) {
+            console.log(e);
+        }
+
+        console.log(myCats);
+    }
+
+    useEffect(() => {
+        getCats();
+    }, []);
+
+    return (
+        <>
+            <LayoutContainer>
+                {isLoading && <Loader />}
+                <div className="flex mx-auto w-full flex-wrap justify-center">
+                    {myCats &&
+                        myCats.map((cat: Cat) => {
+                            return <CatCard cat={cat} key={cat.id} />;
+                        })}
+                </div>
+            </LayoutContainer>
+        </>
+    );
+};
+
+export default MyCats;
