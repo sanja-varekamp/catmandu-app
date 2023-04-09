70643b0ea7649c1ab89a8145a4ffe17e575ad241
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-09 23:08:57 +0200

    make a small variant of the loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import catLoader from './catLoader.gif';
 
-export default function Loader() {
-    return (
-        <div className="flex w-full mt-24 items-center justify-center bg-white">
-            <img src={catLoader} height="80px" width="80px" alt="loading..." />
-        </div>
-    );
+export interface Props {
+  small?: boolean;
 }
+
+const Loader: React.FunctionComponent<Props> = ({ small }: Props) => {
+  return small ? (
+    <div className="flex items-center justify-center bg-white">
+      <img src={catLoader} height="40px" width="40px" alt="loading..." />
+    </div>
+  ) : (
+    <div className="flex w-full mt-24 items-center justify-center bg-white">
+      <img src={catLoader} height="80px" width="80px" alt="loading..." />
+    </div>
+  );
+};
+
+export default Loader;
