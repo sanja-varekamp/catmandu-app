06b1bde3aec9b831e4eb721448531ce995286745
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-09 16:17:55 +0200

    refactor component to accept sub_id as a name.  make the properties div optional

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -4,27 +4,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCat } from '@fortawesome/free-solid-svg-icons';
 
 export interface CatCardProps {
-    cat: Cat;
+  cat: Cat;
+  hasPropertiesSection: boolean;
 }
 
-const CatCard: React.FunctionComponent<CatCardProps> = ({ cat }) => {
-    const breed = cat.breeds[0] || {};
-    return (
-        <div className="border-[4px] border-light-green rounded w-[400px] min-h-[430px] flex m-5 flex-col justify-between">
-            <div className="h-[50px] bg-transparent-pink flex">
-                <p className="text-2xl p-2 ">{breed.name}</p>
-            </div>
-            <div className="items-center flex h-[300px]">
-                <img className="h-full min-w-full object-cover" src={cat.url} alt={breed.name} />
-            </div>
-            <div className="min-h-[80px] bg-transparent-green flex items-center ">
-                <span className="px-2">
-                    <FontAwesomeIcon icon={faCat} size="2xl" />
-                </span>
-                <span className="px-2 text-xl">{breed.temperament}</span>
-            </div>
+const CatCard: React.FunctionComponent<CatCardProps> = ({ cat, hasPropertiesSection }: CatCardProps) => {
+  const breed = cat.breeds[0] || {};
+  return (
+    <div className={`${hasPropertiesSection ? 'min-h-[430px]' : 'min-h-[350px]'} border-[4px] border-light-green rounded w-[400px]  flex m-5 flex-col justify-between`}>
+      <div className="h-[50px] bg-transparent-pink flex">
+        <p className="text-2xl p-2 ">{breed.name ? breed.name : cat.sub_id}</p>
+      </div>
+      <div className="items-center flex h-[300px]">
+        <img className="h-full min-w-full object-cover" src={cat.url} alt={breed.name} />
+      </div>
+      {hasPropertiesSection && (
+        <div className="min-h-[80px] bg-transparent-green flex items-center ">
+          <span className="px-2">
+            <FontAwesomeIcon icon={faCat} size="2xl" />
+          </span>
+          <span className="px-2 text-xl">{breed.temperament}</span>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CatCard;
