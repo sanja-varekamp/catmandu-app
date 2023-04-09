eae4387d7c86e6cad13ca58cebd2f1df9addc52d
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-09 23:09:20 +0200

    implement the upload form

--- src/pages/AddACat.tsx
+++ src/pages/AddACat.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import LayoutContainer from '../components/LayoutContainer';
-import UploadACat from '../components/AddACat/UploadACat';
+import UploadForm from '../components/AddACat/UploadForm';
 import PageTitle from '../components/PageTitle';
 import MyCats from '../components/MyCats';
 
 const AddACat: React.FunctionComponent = () => {
-    return (
-        <LayoutContainer>
-            <PageTitle title="Add A Cat" />
-            <UploadACat />
-            <MyCats />
-        </LayoutContainer>
-    );
+  return (
+    <LayoutContainer>
+      <PageTitle title="Add A Cat" />
+      <UploadForm />
+      <MyCats />
+    </LayoutContainer>
+  );
 };
 
 export default AddACat;
