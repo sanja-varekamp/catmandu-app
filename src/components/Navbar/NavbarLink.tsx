ebb20b9ec474231a8abe4bba40d57b5f30b819a9
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-09 15:35:05 +0200

    refactor the button styling to accomodate the active link from router

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Button } from '../Button';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useMatch, useResolvedPath } from 'react-router-dom';
 
 interface Props {
-    linkText: string;
-    href: string;
-    onClick?: () => void;
+  linkText: string;
+  href: string;
+  onClick?: () => void;
 }
 
 export default function NavbarLink({ linkText, href, onClick }: Props) {
-    // TODO figure out how to add styling for isActive without overwriting the component styling
-    // const resolvedPath = useResolvedPath(href);
-    // const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  const resolvedPath = useResolvedPath(href);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
-    return (
-        <li className="w-fit p-4">
-            <Button onClick={onClick} variant="link" as={NavLink} to={href}>
-                {linkText}
-            </Button>
-        </li>
-    );
+  return (
+    <li className="w-fit p-4">
+      <Button
+        className={`${isActive ? 'text-pink' : 'text-dark-green'}
+        font-bold
+        bg-transparent text-dark-green border-transparent  hover:text-pink m-0 px-1 hover:bg-transparent`}
+        onClick={onClick}
+        as={NavLink}
+        to={href}
+      >
+        {linkText}
+      </Button>
+    </li>
+  );
 }
