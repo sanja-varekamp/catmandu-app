8a663f14b976afe43015af2c10253e256143a2a0
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-10 11:54:29 +0200

    add a delete button to the cat card

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import { Cat } from '../pages/CatGallery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCat } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 export interface CatCardProps {
   cat: Cat;
   hasPropertiesSection: boolean;
+  hasRemoveButton?: boolean;
 }
 
-const CatCard: React.FunctionComponent<CatCardProps> = ({ cat, hasPropertiesSection }: CatCardProps) => {
+const CatCard: React.FunctionComponent<CatCardProps> = ({ cat, hasPropertiesSection, hasRemoveButton }: CatCardProps) => {
   const breed = cat.breeds[0] || {};
   return (
     <div className={`${hasPropertiesSection ? 'min-h-[430px]' : 'min-h-[350px]'} border-[4px] border-light-green rounded w-[400px]  flex m-5 flex-col justify-between`}>
-      <div className="h-[50px] bg-transparent-pink flex">
+      <div className="h-[50px] bg-transparent-pink items-center flex justify-between">
         <p className="text-2xl p-2 ">{breed.name ? breed.name : cat.sub_id}</p>
+        {hasRemoveButton && (
+          <span className="px-2 text-dark-green hover:text-red-500">
+            <FontAwesomeIcon title="Remove image" icon={faTrashCan} />
+          </span>
+        )}
       </div>
       <div className="items-center flex h-[300px]">
         <img className="h-full min-w-full object-cover" src={cat.url} alt={breed.name} />
