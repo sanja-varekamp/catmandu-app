2c734879e9e56bd9584dd2f214c0fd083b0e676b
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-10 15:05:31 +0200

    refactor folder structure

--- src/pages/AddACat.tsx
+++ src/pages/AddACat.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LayoutContainer from '../components/LayoutContainer';
 import UploadForm from '../components/AddACat/UploadForm';
 import PageTitle from '../components/PageTitle';
-import MyCats from '../components/MyCats';
+import MyCats from '../components/AddACat/MyCats';
 
 const AddACat: React.FunctionComponent = () => {
   return (
