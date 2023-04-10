9066f3a2661189d5ea628a5983777bbf1768b8eb
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-11 00:19:32 +0200

    style home page. add responsiveness

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import LayoutContainer from '../components/LayoutContainer';
+import Hero from '../components/Home/Hero';
+import catThread from '../components/Home/catThread.gif';
 
 export interface HomePageProps {}
 
 const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
-    return <div> I am home</div>;
+  return (
+    <LayoutContainer>
+      <div className="flex flex-col-reverse md:flex-row md:mt-24 md:flex-row justify-between">
+        <div className="md: w-50%">
+          <Hero />
+        </div>
+        <div className="md: w-50% justify-between flex flex-col">
+          <div className="w-full flex justify-center p-4">
+            <h1 className="mb-4 text-4xl  font-extrabold md:text-5xl lg:text-6xl">Catmandu - the cat app!</h1>
+          </div>
+
+          <div className="w-full flex justify-end">
+            <div className="w-[60%] flex flex-col">
+              Are we in Nepal yet...?
+              <img src={catThread} className="h-full min-w-full" alt="Cats swinging on a yarn ball" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </LayoutContainer>
+  );
 };
 
 export default HomePage;
