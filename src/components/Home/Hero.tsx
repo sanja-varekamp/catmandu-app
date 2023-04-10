05eefe495afb76be44794549519080f37c6387c5
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-04-11 00:19:03 +0200

    add hero image

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import KathmanduTemple from './KathmanduTemple.png';
+
+export default function Hero() {
+  return (
+    <div>
+      <img src={KathmanduTemple} className="h-full min-w-full" alt="Cartoon drawing of the Swayambhunath Temple" />
+    </div>
+  );
+}
